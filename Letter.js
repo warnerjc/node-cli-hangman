0affa5fe77709e3b37e68e679421a836a77167d6
Jason Warner <jcwarner@gmail.com>
2018-04-15 11:22:28 -0500

    Correct Letter.js to be a proper constructor

--- Letter.js
+++ Letter.js
@@ -1,14 +1,4 @@
-// * **Letter.js**: Contains a constructor, Letter. This constructor should be able to either display an underlying character or a blank placeholder (such as an underscore), depending on whether or not the user has guessed the letter. That means the constructor should define:
-
-//   * A string value to store the underlying character for the letter
-
-//   * A boolean value that stores whether that letter has been guessed yet
-
-//   * A function that returns the underlying character if the letter has been guessed, or a placeholder (like an underscore) if the letter has not been guessed
-
-//   * A function that takes a character as an argument and checks it against the underlying character, updating the stored boolean value to true if it was guessed correctly
-
-function Letter(letter) {
+let Letter = function (letter) {
     this.wordLetter = letter;
     this.guessed = false;
 
@@ -23,3 +13,5 @@ function Letter(letter) {
         }
     }
 }
+
+module.exports = Letter;
